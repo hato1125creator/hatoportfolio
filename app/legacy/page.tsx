ecf9b5683f49f153f653a4738a3252d826864743
hato1125creator <156583876+hato1125creator@users.noreply.github.com>
2026-08-31 15:00:49 +0900

    Preserve previous portfolio as legacy route

---
+++ app/legacy/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function LegacyPortfolio() {
+  redirect("https://hatoportfolio-a8h80bmdv-hato2008125-gmailcoms-projects.vercel.app/");
+}
